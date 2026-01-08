9ffff4a05c7e9ad2df48b1fa5208f193ffd83ee8
Kavinya Mahesh <kavinyamahesh.28@gmail.com>
2026-01-08 18:20:58 +0530

    Two sum problem optimal soln

--- day1.py
+++ day1.py
@@ -1,4 +1,13 @@
 def two_sum(arr, target):
+
+    '''
+    We will select the element of the array one by one using a loop (say i).
+    Then we will check if the other required element (i.e. target - arr[i]) exists in the HashMap.
+    If that element exists, then we will return "YES" for the first variant or we will return the current index i.e. i, and the index of the element found using map i.e. mp[target - arr[i]].
+    If that element does not exist, then we will just store the current element in the HashMap along with its index. Because in the future, the current element might be a part of our answer.
+    Finally, if we are out of the loop, that means there is no such pair whose sum is equal to the target. In this case, we will return either "NO" or {-1, -1} as per the variant of the question.
+    '''
+    
     mp={}
 
     for i , num in enumerate(arr):
