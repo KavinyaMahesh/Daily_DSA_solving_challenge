553ef5bbe0b046afd4dc233673b3cce21bc01c0c
Kavinya Mahesh <kavinyamahesh.28@gmail.com>
2026-06-06 10:45:22 +0530

    'll_1

---
+++ linked_list/1.creating_ll.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self,data,next=None):
+        self.data=data
+        self.next=next
+
+if __name__=="__main__":
+    arr=[1,2,3]
+    y=Node(arr[0])
+    print(y)
+    print(y.data)
